56f82e49f2579df67fa37dcd7c7b2267e9852793
birros <birros@protonmail.com>
2026-04-01 01:40:07 +0200

    fix: advertise WT_MAX_SESSIONS and flow control settings for Safari 26.4

--- protocol.go
+++ protocol.go
@@ -1,11 +1,25 @@
 package webtransport
 
-// settingsEnableWebtransportDraft06 is the value for ENABLE_WEBTRANSPORT
-// that was used up until draft-ietf-webtrans-http3-06.
-const settingsEnableWebtransportDraft06 = 0x2b603742
+const (
+	// settingsEnableWebtransportDraft06 is the value for ENABLE_WEBTRANSPORT
+	// that was used up until draft-ietf-webtrans-http3-06.
+	settingsEnableWebtransportDraft06 = 0x2b603742
 
-// settingsWebTransportEnabled is the value for SETTINGS_WT_ENABLED
-const settingsWebTransportEnabled = 0x2c7cf000
+	// settingsWebTransportEnabled is the value for SETTINGS_WT_ENABLED
+	settingsWebTransportEnabled = 0x2c7cf000
+
+	// settingsWebTransportMaxSessions is the value for SETTINGS_WT_MAX_SESSIONS
+	settingsWebTransportMaxSessions = 0x14e9cd29
+
+	// settingsWebTransportInitialMaxStreamsUni is the value for SETTINGS_WT_INITIAL_MAX_STREAMS_UNI
+	settingsWebTransportInitialMaxStreamsUni = 0x2b64
+
+	// settingsWebTransportInitialMaxStreamsBidi is the value for SETTINGS_WT_INITIAL_MAX_STREAMS_BIDI
+	settingsWebTransportInitialMaxStreamsBidi = 0x2b65
+
+	// settingsWebTransportInitialMaxData is the value for SETTINGS_WT_INITIAL_MAX_DATA
+	settingsWebTransportInitialMaxData = 0x2b61
+)
 
 const (
 	// protocolHeader is the Extended-CONNECT :protocol value per
