27f3e245b5ed8596e5c99ab8a0412bb129ba27f7
birros <birros@protonmail.com>
2026-04-01 01:40:07 +0200

    fix: advertise WT_MAX_SESSIONS and flow control settings for Safari 26.4

--- protocol.go
+++ protocol.go
@@ -7,4 +7,16 @@ const settingsEnableWebtransportDraft06 = 0x2b603742
 // settingsWebTransportEnabled is the value for SETTINGS_WT_ENABLED
 const settingsWebTransportEnabled = 0x2c7cf000
 
+// settingsWebTransportMaxSessions is the value for SETTINGS_WT_MAX_SESSIONS
+const settingsWebTransportMaxSessions = 0x14e9cd29
+
+// settingsWebTransportInitialMaxStreamsUni is the value for SETTINGS_WT_INITIAL_MAX_STREAMS_UNI
+const settingsWebTransportInitialMaxStreamsUni = 0x2b64
+
+// settingsWebTransportInitialMaxStreamsBidi is the value for SETTINGS_WT_INITIAL_MAX_STREAMS_BIDI
+const settingsWebTransportInitialMaxStreamsBidi = 0x2b65
+
+// settingsWebTransportInitialMaxData is the value for SETTINGS_WT_INITIAL_MAX_DATA
+const settingsWebTransportInitialMaxData = 0x2b61
+
 const protocolHeader = "webtransport"
