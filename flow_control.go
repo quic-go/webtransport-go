1216197673c6962094dbc4f3b56f45655823182b
Marten Seemann <martenseemann@gmail.com>
2026-07-18 12:04:47 +0200

    track total data sent on a WebTransport session

---
+++ flow_control.go
@@ -0,0 +1,39 @@
+package webtransport
+
+import "sync"
+
+type outgoingDataFlowController struct {
+	mx sync.Mutex
+	// TODO: Update this and notify waiters when WT_MAX_DATA is implemented.
+	maxData   uint64
+	bytesSent uint64
+	updated   chan struct{}
+}
+
+func newOutgoingDataFlowController(maxData uint64) *outgoingDataFlowController {
+	return &outgoingDataFlowController{
+		maxData: maxData,
+		updated: make(chan struct{}),
+	}
+}
+
+func (f *outgoingDataFlowController) AddBytesSent(n uint64) uint64 {
+	f.mx.Lock()
+	defer f.mx.Unlock()
+
+	var added uint64
+	if f.bytesSent < f.maxData {
+		added = min(n, f.maxData-f.bytesSent)
+	}
+	if added > 0 {
+		f.bytesSent += added
+	}
+	return added
+}
+
+func (f *outgoingDataFlowController) NextUpdate() <-chan struct{} {
+	f.mx.Lock()
+	updated := f.updated
+	f.mx.Unlock()
+	return updated
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.25.0
 
 require (
 	github.com/dunglas/httpsfv v1.1.0
-	github.com/quic-go/quic-go v0.60.1-0.20260711170026-7d1588666fbe
+	github.com/quic-go/quic-go v0.60.1-0.20260718195732-fa20b4618236
 	github.com/stretchr/testify v1.11.1
-	golang.org/x/sync v0.20.0
+	golang.org/x/sync v0.22.0
 )
 
 require (
@@ -14,9 +14,9 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
-	golang.org/x/crypto v0.51.0 // indirect
-	golang.org/x/net v0.55.0 // indirect
-	golang.org/x/sys v0.45.0 // indirect
-	golang.org/x/text v0.37.0 // indirect
+	golang.org/x/crypto v0.54.0 // indirect
+	golang.org/x/net v0.56.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.40.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
